5d1aacbdea64c788c158c7417c73b62b02aba5f4
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-21 18:23:06 -0400

    Some stuff added.

--- src/exceptions.c
+++ src/exceptions.c
@@ -5,16 +5,16 @@ void divisionByZeroExceptionHandle(int signal){
 #include <signal.h>
 #include <stdio.h>
 
-int main(){
-    void (*sigHandle)(int);
-    sigHandle = signal(SIG_ERR,divisionByZeroExceptionHandle);
+// int main(){
+//     void (*sigHandle)(int);
+//     sigHandle = signal(SIG_ERR,divisionByZeroExceptionHandle);
 
-    if (sigHandle == SIG_ERR){
-        perror("Division by zero is undefined.");
-        return 1;
-    }
-    int num1 = 0, num2 = 1;
-    int result = num2 / num1;
-    printf("Result: %d\n", result);
-    return 0;
-}
+//     if (sigHandle == SIG_ERR){
+//         perror("Division by zero is undefined.");
+//         return 1;
+//     }
+//     int num1 = 0, num2 = 1;
+//     int result = num2 / num1;
+//     printf("Result: %d\n", result);
+//     return 0;
+// }
