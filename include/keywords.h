85ef40391073aae213398650007e70af0cdd87f7
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-21 17:28:13 -0400

    Added some keywords

---
+++ include/keywords.h
@@ -0,0 +1,15 @@
+#ifndef __A_CONFIG_KEYWORDS_H
+#define __A_CONFIG_KEYWORDS_H
+
+#include <string.h>
+
+#define BOOLEAN "bool"
+#define INTEGER "int"
+#define FLOAT "float"
+#define STRING "string"
+#define _TRUE "true" // Consider underscore to prevent the name conjuction
+#define _FALSE "false"
+#define _NULL "null"
+
+
+#endif
